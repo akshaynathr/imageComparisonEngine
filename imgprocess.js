d6ab17575db9c61c6c093b51230ae3b22436eebc
akshaynathr <akshaynathr@gmail.com>
2015-07-09 10:53:29 +0530

    Working image processor

--- imgprocess.js
+++ imgprocess.js
@@ -1,11 +1,29 @@
 //function to populate the canvas with images
-function setCanvas(CanvasName){
+//
+function setCanvas(CanvasName,url){
 	var c=document.getElementById(CanvasName);
 	var ctx=c.getContext("2d");
 	//var img=document.getElementById("img1");
 	var img=new Image();
-	img.src="img1"
+	//img.src="img1.jpg"
+	img.src=document.getElementById(url).value;
+	tsrc=document.getElementById(url).value;
 	img.onload=function(){
 	ctx.drawImage(img,0,0,img.width,img.height,0,0,c.width,c.height);}
+	imageprocess(tsrc);
 	}
 
+function imageprocess(img) {
+	
+		var api=resemble(img).onComplete(function(data){
+		console.log(data);
+		});
+
+}
+function process(){
+	var message="Images are ";
+	var end=" % similar";
+	resemble(document.getElementById('url1').value).compareTo(document.getElementById('url2').value).onComplete(function(data) {console.log(data);
+		var similar=100-data.misMatchPercentage;
+		alert(message+similar+end);});
+ }
